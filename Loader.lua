88c4ef765e447f17ae0bf79329cb38127e52cbc5
Cosmic4796 <127627689+Cosmic4796@users.noreply.github.com>
2026-01-03 16:51:57 -0800

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+--[[
+    Da Hood Script Loader
+    Usage: loadstring(game:HttpGet('https://raw.githubusercontent.com/YOUR_USERNAME/DaHoodScript/main/Loader.lua'))()
+]]
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/YOUR_USERNAME/DaHoodScript/main/Main.lua'))()
