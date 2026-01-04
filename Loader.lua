8b5aaf4d81fceb22e977ff7a4754d746a9bfa2d8
Cosmic4796 <127627689+Cosmic4796@users.noreply.github.com>
2026-01-03 16:52:44 -0800

    Update Da Hood Script Loader URL

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,7 @@
 --[[
     Da Hood Script Loader
-    Usage: loadstring(game:HttpGet('https://raw.githubusercontent.com/YOUR_USERNAME/DaHoodScript/main/Loader.lua'))()
+    Usage: loadstring(game:HttpGet('https://raw.githubusercontent.com/Cosmic4796/DaHoodScript/main/Loader.lua'))()
 ]]
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/YOUR_USERNAME/DaHoodScript/main/Main.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Cosmic4796/DaHoodScript/main/Main.lua'))()
+
